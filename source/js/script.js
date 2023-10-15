bb81d0e29a65685aee30c9dbdf942ec0b447c16c
Georgij Tsábiev <gtsabiev@inbox.ru>
2023-10-15 13:44:43 +0300

    Подключает интерактивную карту с кастомным маркером

--- source/js/script.js
+++ source/js/script.js
@@ -16,3 +16,41 @@ navToggle.addEventListener('click', function () {
     nav.classList.remove('nav--opened');
   }
 });
+
+/* Карта */
+
+let screenWidth = window.screen.width;
+let mapWrapper = document.querySelector('.map__wrapper');
+let mapImage = document.querySelector('.map__image');
+let center = [34.866849964021355,-111.76106949186402];
+let pinSize = [27, 27];
+let pinOffset = [-10, -5];
+let zoom = 7;
+
+mapWrapper.classList.remove('map__wrapper--no-js');
+mapImage.classList.remove('map__image--no-js');
+
+function init() {
+  let map = new ymaps.Map('map', {
+    center: center,
+    zoom: zoom
+  });
+
+  let placemark = new ymaps.Placemark(center, {}, {
+    iconLayout: 'default#image',
+    iconImageHref: 'img/map-pin.svg',
+    iconImageSize: pinSize,
+    iconImageOffset: pinOffset
+  });
+
+  map.controls.remove("geolocationControl");
+  map.controls.remove("searchControl");
+  map.controls.remove("trafficControl");
+  map.controls.remove("typeSelector");
+  map.controls.remove("fullscreenControl");
+  map.controls.remove("zoomControl");
+  map.controls.remove("rulerControl");
+  map.geoObjects.add(placemark);
+}
+
+ymaps.ready(init);
